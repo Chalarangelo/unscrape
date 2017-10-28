149a28cdfa63cd00e35c56c59f289a6fc4310a1d
Angelos Chalaris <chalarangelo@gmail.com>
2017-10-28 21:59:31 +0300

    Downloading images

--- unscrape.py
+++ unscrape.py
@@ -1,5 +1,8 @@
+import requests
 import time
 from selenium import webdriver
+from PIL import Image
+from io import BytesIO
 
 url = "https://unsplash.com"
 
@@ -9,6 +12,10 @@ driver.get(url)
 driver.execute_script("window.scrollTo(0,1000);")
 time.sleep(5)
 image_elements = driver.find_elements_by_css_selector("#gridMulti img")
+i = 0
 for image_element in image_elements:
     image_url = image_element.get_attribute("src")
-    print(image_url)
+    image_object = requests.get(image_url)
+    image = Image.open(BytesIO(image_object.content))
+    image.save("./images/image" + str(i) + "." + image.format, image.format)
+    i += 1
