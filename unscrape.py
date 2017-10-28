15d1edff1fade61fc996eeb5a2780534a3ef53ce
Angelos Chalaris <chalarangelo@gmail.com>
2017-10-28 21:22:03 +0300

    Scrolling the page

--- unscrape.py
+++ unscrape.py
@@ -1,6 +1,10 @@
+import time
 from selenium import webdriver
 
 url = "https://unsplash.com"
 
 driver = webdriver.Firefox(executable_path=r'geckodriver.exe')
 driver.get(url)
+
+driver.execute_script("window.scrollTo(0,1000);")
+time.sleep(5)
