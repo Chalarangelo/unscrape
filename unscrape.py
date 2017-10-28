c411a863ba5d56c479c12c13c852396b0f94152d
Angelos Chalaris <chalarangelo@gmail.com>
2017-10-28 21:47:09 +0300

    Selecting image elements and getting the image URLs

--- unscrape.py
+++ unscrape.py
@@ -8,3 +8,7 @@ driver.get(url)
 
 driver.execute_script("window.scrollTo(0,1000);")
 time.sleep(5)
+image_elements = driver.find_elements_by_css_selector("#gridMulti img")
+for image_element in image_elements:
+    image_url = image_element.get_attribute("src")
+    print(image_url)
